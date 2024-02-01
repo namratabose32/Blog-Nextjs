8cd5755a0a16d47ff1bcd3db4607022f6698fa81
Namrata <namratabose32@gmail.com>
2024-02-01 21:46:42 +0530

    feat:Navbar created

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import AuthLinks from "../authLinks/AuthLinks";
+import ThemeToggle from "../ThemeToggle/ThemeToggle";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+        <Image src="/instagram.png" alt="instagram" width={24} height={24} />
+        <Image src="/youtube.png" alt="youtube" width={24} height={24} />
+      </div>
+      <div className={styles.logo}> logo</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/">Homepage</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">About</Link>
+        <AuthLinks />
+      </div>
+    </div>
+  );
+};
+export default Navbar;
