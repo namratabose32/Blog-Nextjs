26dd6ad49a855c7d684f4d041b13131f76cba780
Namrata <namratabose32@gmail.com>
2024-02-02 19:03:02 +0530

    feat:Authentication

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,21 +1,52 @@
-import React from "react";
-import styles from "./authLinks.module.css";
+"use client";
 import Link from "next/link";
-function AuthLinks() {
-  //temporary
-  const status = "authenticated";
+import styles from "./authLinks.module.css";
+import { useState } from "react";
+import { signOut, useSession } from "next-auth/react";
+
+const AuthLinks = () => {
+  const [open, setOpen] = useState(false);
+
+  const { status } = useSession();
+
   return (
     <>
-      {status === "notauthenticated" ? (
-        <Link href="/login">Login</Link>
+      {status === "unauthenticated" ? (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
       ) : (
         <>
-          <Link href="/write">Write</Link>
-          <span className={styles.link}>Logout</span>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
+          <span className={styles.link} onClick={signOut}>
+            Logout
+          </span>
         </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          {status === "notauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
+        </div>
       )}
     </>
   );
-}
+};
 
 export default AuthLinks;
---
+++ src/utils/connect.js
@@ -0,0 +1,14 @@
+//check if we have created client before or not
+
+import { PrismaClient } from "@prisma/client";
+let prisma;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
